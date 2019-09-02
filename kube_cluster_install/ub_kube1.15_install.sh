84b5ecad3598184c8c722679d36e85d27c74f390
root <root@kube1.192.168.54.140.nip.io>
2019-09-02 12:06:04 +0000

    fir

---
+++ kube_cluster_install/ub_kube1.15_install.sh
@@ -0,0 +1,24 @@
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+systemctl restart docker
+swapoff -a
+#vi fastab also
+kubeadm init --pod-network-cidr=192.168.0.0/16 > /kube_join.txt
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
