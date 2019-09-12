d5272251d8384498bad2dcdcd411cdaefa10f296
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2019-09-12 21:32:46 +0530

    Update kinstall.sh

--- kube_cluster_install/kinstall.sh
+++ kube_cluster_install/kinstall.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install -y apt-transport-https curl
+apt-get update && apt-get install -y apt-transport-https curl docker.io
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
