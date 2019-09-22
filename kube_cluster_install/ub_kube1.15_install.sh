3dc4f54a0bad6275bf93c21830c1f2bd79c4a02e
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2019-09-22 16:32:43 +0530

    Update ub_kube1.15_install.sh

--- kube_cluster_install/ub_kube1.15_install.sh
+++ kube_cluster_install/ub_kube1.15_install.sh
@@ -38,7 +38,23 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/62e44c867a2846
     
     
     
-    
+ cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+systemctl enable --now kubelet   
     
     
 firewall-cmd --permanent --add-port=6443/tcp
